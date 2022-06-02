0debbe0b112ef32bc2f3bf950a366f7b64fea790
小黄 <xiaohuang011214@163.com>
2022-06-02 10:40:52 +0800

    新建

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * Date: 2022/6/2
+ * Author:George
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

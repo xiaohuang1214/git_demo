3a0170871dc19d4467c37048dce832a88deb199a
小黄 <xiaohuang011214@163.com>
2022-06-02 10:42:13 +0800

    第二次更新

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("第一次更新");
+        System.out.println("第二次更新");
     }
 }
